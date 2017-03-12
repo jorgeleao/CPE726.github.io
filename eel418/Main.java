5a3be985f245e83f0f463270d38647166e4d8266
jorgeleao <jorge.leao@ufrj.br>
2017-03-12 14:41:24 -0300

    aaa

---
+++ eel418/Main.java
@@ -0,0 +1,16 @@
+package p1;
+
+// Compilação: javac -d  ./ Main.java
+// Execução:   java  -cp ./ p1.Main
+
+public class Main {
+    int contador;
+    
+    public static void main(String[] argumentos){
+        System.out.println("\n\n--- Funcionou: "+Math.PI+"\n\n");
+    }
+}
+
+class Aoutra{
+	int contador = 0;
+}
